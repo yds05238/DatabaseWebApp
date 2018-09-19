db470a7b7fa082aed3cd2e9a431c589338cac17d
yds05238 <jy392@cornell.edu>
2018-09-19 04:04:40 -0400

    changes 1234

--- project/js/main.js
+++ project/js/main.js
@@ -1,11 +1,3 @@
-// Welcome!
-
-
-//Read this file from bottom to top!!
-
-
-// This function sets up a listener- '.on()' gets called automatically whenever something saved in '/stream/' changes.
-// It's main purpose is to iterate over the stream in the database and add each message to the page.
 function initializeStreamListener() {
   const databaseStreamReference = firebase.database().ref('/stream/');
 
@@ -22,8 +14,6 @@ function initializeStreamListener() {
   });
 }
 
-// This function gets called with the new message information.
-// It gets the user information and uses both to add the post to the database.
 function addMessage(body, title) {
   var user = firebase.auth().currentUser;
   var authorPic = user.photoURL;
@@ -41,8 +31,6 @@ function addMessage(body, title) {
 }
 
 
-// This gets called whenver the form is submitted (check out the index.ejs).
-// Uses jQuery to get the message info and passes it to 'addMessage to actually submit the info to firebase.
 function handleMessageFormSubmit() {
   var body = $('#new-post-body').val();
   var title = $('#new-post-title').val();
@@ -50,9 +38,9 @@ function handleMessageFormSubmit() {
   addMessage(body, title);
 }
 
-// Gets called whenever the user clicks "sign in" or "sign out".
-/**function toggleSignIn() {
-  if (!firebase.auth().currentUser) { // if no user, handle login
+
+function toggleSignIn() {
+  if (!firebase.auth().currentUser) { 
     var provider = new firebase.auth.GoogleAuthProvider();
     provider.addScope("https://www.googleapis.com/auth/plus.login");
     firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -60,39 +48,14 @@ function handleMessageFormSubmit() {
     }).catch(function(error) {
       console.error("error", error);
     });
-  } else { // handle logout
+  } else { 
     firebase.auth().signOut();
   }
 
-  //This disables the button until login or logout is successful
   $('#login-button').attr("disabled", true);
-}*/
-function toggleSignIn() {
-  if (!firebase.auth().currentUser) {
-    // [START createprovider]
-    var provider = new firebase.auth.GoogleAuthProvider();
-    // [END createprovider]
-    // [START addscopes]
-    provider.addScope('https://www.googleapis.com/auth/plus.login');
-    // [END addscopes]
-    // [START signin]
-    firebase.auth().signInWithPopup(provider);
-    // [END signin]
-  } else {
-    // [START signout]
-    firebase.auth().signOut();
-    // [END signout]
-  }
-  // [START_EXCLUDE]
-  document.getElementById('quickstart-sign-in').disabled = true;
-  // [END_EXCLUDE]
 }
 
 
-
-// The main purpose of this function is to set up a listener (using firebase) for when the auth state changes.
-// If a user isn't authenticated, we should not show the stream and prompt them to log in.
-// If a use IS authenticated, we should load/show the stream and give them the option to log out.
 window.onload = function() {
   firebase.auth().onAuthStateChanged(function(user){
     if (user) {
--- project/start.js
+++ project/start.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname));
 
-// views is directory for all template files
+
 app.set('views', __dirname + '/html');
 app.set('view engine', 'ejs');
 
@@ -18,5 +18,3 @@ app.listen(app.get('port'), function() {
 });
 
 
-// This file is what handles incoming requests and
-// serves files to the browser, or executes server-side code
